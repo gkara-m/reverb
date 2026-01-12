4b1cd47b486ec2805d57489446828a4807a2f3c3
amber-orton <ambertcorton@gmail.com>
2026-01-12 11:35:47 +0000

    refactor: playlist cli handling moves to a fn

--- src/ui/cli.rs
+++ src/ui/cli.rs
@@ -19,7 +19,7 @@ pub fn get_input() -> String {
     
     fn run_command(input: String, internal: &mut Internal) -> bool {
         match input.trim().split_once(" ") {
-        Some((command, param)) => {command_check_composite(command, param, internal)}
+        Some((command, args)) => {command_check_composite(command, args, internal)}
         None => {command_check_single(input.trim(), internal)}
     }
 }
@@ -48,76 +48,91 @@ fn command_check_single(command: &str, internal: &mut Internal) -> bool {
     false
 }
 
-fn command_check_composite(command: &str, param: &str, internal: &mut Internal) -> bool {
+fn command_check_composite(command: &str, args: &str, internal: &mut Internal) -> bool {
     match command {
         "play-new" => {
-            match Song::new(param) {
+            match Song::new(args) {
                 Some(song) => {internal.play_new(song);},
                 None => {invalid_input();}
             }
         }
         "queue-add" => {
-            match Song::new(param) {
+            match Song::new(args) {
                 Some(song) => {internal.queue_add(song);}
                 None => {invalid_input();}
             }
         }
         "queue-remove" => {
-            internal.queue_remove(param.parse().expect("Please enter valid song index"));
+            internal.queue_remove(args.parse().expect("Please enter valid song index"));
         }
-        "playlist-new" => {
-            match param.split_once(" ") {
-                Some((name, external_type)) => {
-                    match ExternalType::get_from_str(external_type) {
-                        Some(external_type) => {
-                            if !internal.new_playlist(name, Some(external_type)) {
-                                print!("ahhhhh");
-                            }
-                        }
-                        None => {invalid_input();}
-                    }
-                }
-                None => {internal.new_playlist(param, None);}
-            }
-        }
-        "playlist-add" => {
-            match Song::new(param) {
-                Some(song) => {internal.playlist_add(song);}
-                None => {invalid_input();}
-            }
-        }
-        "playlist-remove" => {
-            match param.parse() {
-                Ok(index) => {internal.playlist_remove(index);}
-                Err(_) => {invalid_input();}
-            }
-        }
-        "playlist-load" => {
-            if !internal.load_playlist(param) {
-                print!("ahhhhh");
-            }
-        }
-        "playlist-move" => {
-            match param.split_once(" ") {
-                Some((from_str, to_str)) => {
-                    match (from_str.parse(), to_str.parse()) {
-                        (Ok(from), Ok(to)) => {internal.playlist_move_song(from, to);}
-                        _ => {invalid_input();}
-                    }
-                }
-                None => {invalid_input();}
-            }
-        }
-        "playlist-list" => {
-            let songs = internal.playlist_get_songs();
-            for (index, song) in songs.iter().enumerate() {
-                println!("{}: {} - {}", index, song.artist, song.title);
-            }
+        "playlist" => {
+            handle_playlist(internal, args);
         }
         _ => {invalid_input();}
     }
     false
 }
+
+fn handle_playlist(internal: &mut Internal, args: &str) {
+    match args.split_once(" ") {
+        Some((action, args)) => {
+            match action {
+                "add" => {
+                    match Song::new(args) {
+                        Some(song) => {internal.playlist_add(song);}
+                        None => {invalid_input();}
+                    }
+                }
+                "remove" => {
+                    match args.parse() {
+                        Ok(index) => {internal.playlist_remove(index);}
+                        Err(_) => {invalid_input();}
+                    }
+                }
+                "load" => {
+                    if !internal.load_playlist(args) {
+                        print!("ahhhhh");
+                    }
+                }
+                "move" => {
+                    match args.split_once(" ") {
+                        Some((from_str, to_str)) => {
+                            match (from_str.parse(), to_str.parse()) {
+                                (Ok(from), Ok(to)) => {internal.playlist_move_song(from, to);}
+                                _ => {invalid_input();}
+                            }
+                        }
+                        None => {invalid_input();}
+                    }
+                }
+                "list" => {
+                    let songs = internal.playlist_get_songs();
+                    for (index, song) in songs.iter().enumerate() {
+                        println!("{}: {} - {}", index, song.artist, song.title);
+                    }
+                }
+                "new" => {
+                    match args.split_once(" ") {
+                        Some((name, external_type)) => {
+                            match ExternalType::get_from_str(external_type) {
+                                Some(external_type) => {
+                                    if !internal.new_playlist(name, Some(external_type)) {
+                                        print!("ahhhhh");
+                                    }
+                                }
+                                None => {invalid_input();}
+                            }
+                        }
+                        None => {internal.new_playlist(args, None);}
+                    }
+                }
+                _ => {invalid_input();}
+            }
+        }
+        None => {invalid_input();}
+    }
+}
+                
 
 fn invalid_input() {
     println!("Invalid Input");
