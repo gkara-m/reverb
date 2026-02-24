63c72c638975df858334f2c03265ca0a1f21a8eb
amber-orton <ambertcorton@gmail.com>
2026-02-24 01:50:40 +0000

    refactor: failure format

--- src/failure/failure.rs
+++ src/failure/failure.rs
@@ -1,6 +1,6 @@
 pub enum Failure {
-    Error(FailureError),
-    Warning(FailureError),
+    Fetal(std::io::Error),
+    Warning(std::io::Error),
 }
 
 pub enum FailureType {
@@ -9,53 +9,45 @@ pub enum FailureType {
 }
 
 impl Failure {
-    fn message(&self) -> &str {
-        self.unwrap().message()
-    }
-    fn std_err(&self) -> &Option<std::io::Error> {
-        self.unwrap().std_err()
-    }
-    fn is_std_err(&self) -> bool {
-        self.std_err().is_some()
+    fn message(&self) -> String {
+        self.unwrap().to_string()
     }
 
-    pub fn from(std_err: Option<std::io::Error>, message: Option<String>, failure_t: FailureType) -> Self {
-        match failure_t {
-            FailureType::Error => Failure::Error(FailureError::from(std_err, message)),
-            FailureType::Warning => Failure::Warning(FailureError::from(std_err, message)),
+    fn is_error(&self) -> bool {
+        match self {
+            Failure::Fetal(_) => true,
+            Failure::Warning(_) => false,
         }
     }
 
-    fn unwrap(&self) -> &FailureError {
+    fn std_err(&self) -> &std::io::Error {
+        self.unwrap()
+    }
+
+    fn unwrap(&self) -> &std::io::Error {
         match self {
-            Failure::Error(e) => e,
+            Failure::Fetal(e) => e,
             Failure::Warning(w) => w,
         }
     }
 }
 
-pub struct FailureError {
-    message: String,
-    std_err: Option<std::io::Error>,
+impl From<(std::sync::mpsc::SendError<crate::Command>, FailureType)> for Failure {
+    fn from((err, failure_type): (std::sync::mpsc::SendError<crate::Command>, FailureType)) -> Self {
+        let msg = format!("Failed to send on channel: {}", err);
+        match failure_type {
+            FailureType::Error => Failure::Fetal(std::io::Error::new(std::io::ErrorKind::BrokenPipe, msg)),
+            FailureType::Warning => Failure::Warning(std::io::Error::new(std::io::ErrorKind::BrokenPipe, msg)),
+        }
+    }
 }
 
-
-impl FailureError {
-    fn message(&self) -> &str {
-        &self.message
-    }
-
-    fn std_err(&self) -> &Option<std::io::Error> {
-        &self.std_err
-    }
-
-    fn from(std_err: Option<std::io::Error>, message: Option<String>) -> Self {
-        let message = match (std_err.as_ref(), message) {
-            (Some(std_err), None) => std_err.to_string(),
-            (None, Some(message)) => message,
-            (Some(std_err), Some(message)) => format!("{}: {}", message, std_err),
-            (None, None) => panic!("FailureError must have either a std_err or a message"),
-        };
-        FailureError { message, std_err }
+impl From<(std::sync::mpsc::RecvError, FailureType)> for Failure {
+    fn from((err, failure_type): (std::sync::mpsc::RecvError, FailureType)) -> Self {
+        let msg = format!("Failed to receive command response: {}", err);
+        match failure_type {
+            FailureType::Error => Failure::Fetal(std::io::Error::new(std::io::ErrorKind::BrokenPipe, msg)),
+            FailureType::Warning => Failure::Warning(std::io::Error::new(std::io::ErrorKind::BrokenPipe, msg)),
+        }
     }
 }
