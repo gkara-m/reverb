c41b92fec353b44c0d43782c43034ef90c8bc0f0
amber-orton <ambertcorton@gmail.com>
2026-03-02 01:52:41 +0000

    refactor: cli_ui queue into function calls

--- src/ui/cli/cli_ui.rs
+++ src/ui/cli/cli_ui.rs
@@ -1,8 +1,8 @@
 use std::{io::{self, BufRead, Write}, sync::mpsc::Sender};
 
-use crossterm::{cursor, queue, style::Print, terminal::{self, ClearType}};
+use crossterm::{cursor, queue, style::Print, terminal::{self}};
 
-use crate::{Command, failure::failure::{Failure, FailureType}, ui::{cli::cli::print_failure, ui}};
+use crate::{Command, failure::failure::{Failure, FailureType}, internal::song, ui::{cli::cli::print_failure, ui}};
 
 pub(super) fn run_ui(transmit: &Sender<Command>, input_tx: Sender<String>, update_interval: u64) -> std::thread::JoinHandle<()> {
     let main_transmit = transmit.clone();
@@ -128,6 +128,10 @@ fn queue_song_name(width: u16, position: (u16, u16), stdout: &mut std::io::Stdou
 // create and queue the queue text, truncating as necessary to fit the width and height
 // this looks 
 fn queue_queue(size: (u16, u16), position: (u16, u16), stdout: &mut std::io::Stdout, transmit: &Sender<Command>) -> Result<(), Failure> {
+    if size.1 == 0 || size.0 == 0 {
+        return Ok(());
+    }
+    
     let queue = ui::queue_get_songs(transmit)?;
     let mut queue_iter = queue.iter();
     let mut queue_text = Vec::new();
@@ -138,52 +142,20 @@ fn queue_queue(size: (u16, u16), position: (u16, u16), stdout: &mut std::io::Std
 
                 // add name
                 let name = &song.info.title;
-                if name.chars().count() as u16 > size.0 {
-                    let mut truncated = name.chars().take((size.0 - 3) as usize).collect::<String>();
-                    truncated.push_str("...");
-                    queue_text.push(truncated);
-
-                } else {
-                    song_text.push_str(name);
-                }
+                push_helper(&mut song_text, name, "", "", &mut queue_text, size.0);
 
                 // add artist
                 let artist = &song.info.artist;
-                if song_text.chars().count() as u16 + artist.chars().count() as u16 + 3 > size.0 {
-                    queue_text.push(song_text.clone());
-                    song_text.clear();
-                    if artist.chars().count() as u16 + "  ".chars().count() as u16 > size.0 as u16 {
-                        let mut truncated = "  ".to_string();
-                        truncated.push_str(&artist.chars().take((size.0 - "  ".chars().count() as u16 - 3) as usize).collect::<String>());
-                        truncated.push_str("...");
-                        queue_text.push(truncated);
-                    } else {
-                        song_text.push_str(&format!("  {}", artist));
-                    }
-                } else {
-                    song_text.push_str(" - ");
-                    song_text.push_str(artist);
-                }
+                push_helper(&mut song_text, artist, "  ", " - ", &mut queue_text, size.0);
+
 
                 // add type
-                let external_type = &song.song_type.as_type();
-                if song_text.chars().count() as u16 + external_type.to_string().chars().count() as u16 + 3 > size.0 {
-                    queue_text.push(song_text.clone());
-                    song_text.clear();
-                    if external_type.to_string().chars().count() as u16 + "  ()".chars().count() as u16 > size.0 as u16 {
-                        let mut truncated = "  (".to_string();
-                        truncated.push_str(&external_type.to_string().chars().take((size.0 - truncated.chars().count() as u16 - 3) as usize).collect::<String>());
-                        truncated.push_str("...");
-                        queue_text.push(truncated);
-                    } else {
-                        song_text.push_str(&format!("  ({})", external_type.to_string()));
-                    }
-                } else {
-                    song_text.push_str("  (");
-                    song_text.push_str(&external_type.to_string());
-                    song_text.push_str(")");
+                let external_type = format!("({})", song.song_type.as_type());
+                push_helper(&mut song_text, &external_type.to_string(), "  ", "  ", &mut queue_text, size.0);
+
+                if song_text.chars().count() > 0 {
+                    queue_text.push(song_text);
                 }
-                queue_text.push(song_text);
             },
             None => {
                 queue_text.push(" ".to_string());
@@ -204,4 +176,21 @@ fn queue_queue(size: (u16, u16), position: (u16, u16), stdout: &mut std::io::Std
     }
 
     Ok(())
+}
+
+fn push_helper(string: &mut String, to_push: &str, new_line_start: &str, same_line_separator: &str, list: &mut Vec<String>, max_length: u16) {
+    if string.chars().count() as u16 + to_push.chars().count() as u16 + 3 > max_length {
+        list.push(string.clone());
+        string.clear();
+        if to_push.chars().count() as u16 + new_line_start.chars().count() as u16 > max_length as u16 {
+            let mut truncated = new_line_start.to_string();
+            truncated.push_str(&to_push.chars().take((max_length - new_line_start.chars().count() as u16 - 3) as usize).collect::<String>());
+            truncated.push_str("...");
+            list.push(truncated);
+        } else {
+            string.push_str(&format!("{}{}", new_line_start, to_push));
+        }
+    } else {
+        string.push_str(&format!("{}{}", same_line_separator, to_push));
+    }
 }
