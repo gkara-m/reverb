482caac1cef69d7b25bb977bc353ca8f8ae13acb
amber-orton <ambertcorton@gmail.com>
2026-03-23 00:42:15 +0000

    refactor: client internet into more functions

--- crates/reverb-client/src/internal/internet.rs
+++ crates/reverb-client/src/internal/internet.rs
@@ -4,6 +4,7 @@ use anyhow::{Result, anyhow};
 use quinn::Connection;
 use quinn_proto::crypto::rustls::QuicClientConfig;
 use rustls::pki_types::CertificateDer;
+use std::net::AddrParseError;
 
 use std::sync::Arc;
 
@@ -51,11 +52,12 @@ async fn connect_to(server: String) -> Result<Connection, Failure> {
 
     // Wrap the client crypto config in a Quinn QUIC client config
     let client_config =
-        quinn::ClientConfig::new(Arc::new(QuicClientConfig::try_from(client_crypto)?));
+        quinn::ClientConfig::new(Arc::new(QuicClientConfig::try_from(client_crypto)
+        .map_err(|e| Failure::from((e.into(), FailureType::Warning)))?));
     // Create a new QUIC endpoint for the client, binding to an ephemeral port on all interfaces
     let mut endpoint = quinn::Endpoint::client("[::]:0".parse()
-        .map_err(|e| Failure::from((anyhow!(e), FailureType::Warning)))?)
-        .map_err(|e| Failure::from((anyhow!(e), FailureType::Warning)))?;
+        .map_err(|e: AddrParseError| Failure::from((e.into(), FailureType::Warning)))?)
+        .map_err(|e| Failure::from((e.into(), FailureType::Warning)))?;
     // Set the default client configuration for outgoing connections
     endpoint.set_default_client_config(client_config);
 
@@ -69,13 +71,14 @@ async fn connect_to(server: String) -> Result<Connection, Failure> {
     // Parse the server address string into a SocketAddr
     let remote = server
         .parse::<SocketAddr>()
-        .map_err(|_| anyhow!("Invalid address"))?;
+        .map_err(|e| Failure::from((e.into(), FailureType::Warning)))?;
 
     // Initiate a QUIC connection to the server with the given address and host
     let conn = endpoint
-        .connect(remote, host)?
+        .connect(remote, host)
+        .map_err(|e| Failure::from((e.into(), FailureType::Warning)))?
         .await
-        .map_err(|e| anyhow!("Connection failed: {}", e))?;
+        .map_err(|e| Failure::from((e.into(), FailureType::Warning)))?;
 
     Ok(conn)
 }
@@ -88,12 +91,15 @@ async fn run(server: String, message: String) -> Result<(), Failure> {
     println!("Connected, opening stream");
 
     // Open a bidirectional stream to the server
-    let (mut send, mut recv) = conn.open_bi().await?;
+    let (mut send, mut recv) = conn.open_bi().await
+    .map_err(|e| Failure::from((e.into(), FailureType::Warning)))?;
 
     // Send the message to the server
-    send.write_all(message.as_bytes()).await?;
+    send.write_all(message.as_bytes()).await
+    .map_err(|e| Failure::from((e.into(), FailureType::Warning)))?;
     // Indicate that no more data will be sent on this stream
-    send.finish()?;
+    send.finish()
+    .map_err(|e| Failure::from((e.into(), FailureType::Warning)))?;
     println!("Sent: {}", message);
 
     // Wait for the server's response and print it
